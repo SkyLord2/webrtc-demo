a7e0a1745d691a6138c6e7a58b82e6e7dd3739f5
SkyLord2 <chengdongsheng@outlook.com>
2022-02-13 20:03:27 +0800

    webrtc拉流

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,56 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <div @click="play" class="btn">播放视频</div>
+        <video id="rtc_media_player" controls autoplay></video>
+        <div><span>Session id:</span><span>{{sessionid}}</span></div>
+        <div><span>Simulator:</span><span>{{simulator}}</span></div>
+    </div>
 </template>
 
 <script>
+import { SrsRtcPlayerAsync } from '../libs/srs.sdk';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    data() {
+        return {
+            sdk: null,
+            sessionid: '',
+            simulator: ''
+        };
+    },
+    props: {
+        msg: String
+    },
+    methods: {
+        play() {
+            var video = document.getElementById('rtc_media_player');
+            // Close PC when user replay.
+            if (this.sdk) {
+                this.sdk.close();
+            }
+            this.sdk = new SrsRtcPlayerAsync();
+
+            // https://webrtc.org/getting-started/remote-streams
+            video.srcObject = this.sdk.stream;
+            // Optional callback, SDK will add track to stream.
+            // sdk.ontrack = function (event) { console.log('Got track', event); sdk.stream.addTrack(event.track); };
+
+            // For example: webrtc://r.ossrs.net/live/livestream
+            var url = 'webrtc://124.222.18.231:80/live/livestream';
+            this.sdk.play(url).then((session) => {
+                this.sessionid = session.sessionid;
+                this.simulator = session.simulator;
+            }).catch((reason) => {
+                this.sdk.close();
+                console.error(reason);
+            });
+        },
+        stop() {
+            var video = document.getElementById('rtc_media_player');
+            video.srcObject = null;
+            video.pause();
+        }
+    },
 }
 </script>
 
@@ -55,5 +69,13 @@ li {
 }
 a {
   color: #42b983;
+}
+.btn {
+    width: 70px;
+    border: 1px solid #42b983;
+    background-color: #42b983;
+    cursor: pointer;
+    margin: auto;
+    margin-bottom: 20px;
 }
 </style>
